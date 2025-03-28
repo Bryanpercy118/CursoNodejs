74b0a270642bb582d5a68577c3bc45b732031c0c
BRYAN ANDRES GRANADOS PERCY <bagranados@unicesar.edu.co>
2025-03-28 15:13:51 -0500

    implementacion de nodemon, componentes y module export

---
+++ 02-bases/src/js-foundation/01-template.js
@@ -0,0 +1,14 @@
+
+const emailTemplate = `
+    <div>
+        <h1>Welcome to Our Service!</h1>
+        <p>Dear {{name}},</p>
+        <p>Thank you for signing up for our service. We are excited to have you on board!</p>
+        <p>Best regards,</p>
+        <p>The Team</p>
+    </div>
+`;
+
+module.exports = {
+    emailTemplate,
+};
