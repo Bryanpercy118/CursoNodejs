dce5fe9c69cc7d94679667b7c8d3467d6c77ecd9
BRYAN ANDRES GRANADOS PERCY <bagranados@unicesar.edu.co>
2025-03-31 09:11:05 -0500

    changes

--- 02-bases/src/app.js
+++ 02-bases/src/app.js
@@ -1,13 +1,15 @@
 // const { emailTemplate } = require('./js-foundation/01-template');
 // require ('./js-foundation/02-destructuring');
 //const {getUserById} = require ('./js-foundation/04-arrow');
-require ('./js-foundation/05-factory');
-// console.log(emailTemplate)
+// const { getUUID, getAge } = require('./plugins') factory
+// const { buildMakePerson } = require ('./js-foundation/05-factory');
+// const makePerson = buildMakePerson({ getUUID, getAge });
+// const person = { name: 'Bryan Percy', birthdate: '1998-03-24' };
+// const newPerson = makePerson(person);
+// console.log(newPerson); fin del factory
 
-//const id = 1;
-// getUserById(id, (error, user)=> {
-//     if(error){
-//         throw new Error(error);
-//     }
-//     console.log(user);
-// })
+const getPokemonById = require('./js-foundation/06-promises');
+
+getPokemonById(1)
+    .then((pokemon) => console.log(pokemon))
+    .catch((error) => console.error(error));
--- 02-bases/src/js-foundation/05-factory.js
+++ 02-bases/src/js-foundation/05-factory.js
@@ -1,17 +1,20 @@
 //es una funcion que crea otra funcion
 // const { getUUID } = require('../plugins/get-id.plugin');
 // const { getAge } = require('../plugins/get-age.plugin');
-const {getUUID, getAge} = require('../plugins')
-const buildPerson = ({ name, birthdate }) => {
-    return {
-        id: getUUID(),
-        name: name,
-        birthdate: birthdate,
-        age: getAge(birthdate),
+//const {getUUID, getAge} = require('../plugins')
+
+const buildMakePerson = ( { getUUID, getAge }) => {
+    return ({ name, birthdate }) => {
+        return {
+            id: getUUID(),
+            name: name,
+            birthdate: birthdate,
+            age: getAge(birthdate),
+        }
     }
 }
 
-const obj ={ name: 'John', birthdate:'1998-03-24'}
-const John = buildPerson( obj );
 
-console.log(John);
+module.exports = {
+    buildMakePerson,
+}
